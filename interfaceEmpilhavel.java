2caaddaee8f510a4475815bc970136ad9e3c5953
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-03-10 23:35:06 -0300

    inicialização do repositório, com base no que foi apresentado na primeira aula, ministrada dia 10/03/2025

---
+++ interfaceEmpilhavel.java
@@ -0,0 +1,56 @@
+/*
+TIPOS DE DADOS
+
+
+	Numerico (positivo ou negativo)
+		Inteiros
+		unsigned int
+		tinyint, smallint
+		Int
+		Long
+	Pontos flutuantes
+		Float
+		Double
+	Ponto fixo (nunca ouvi falar)
+		Numerical/Decimal (útil em SQL)
+
+	
+	
+	String = ""
+	char = ''
+
+	Logico 
+		Boolean, bool
+
+	Matriz unidimensional
+		Vector, array, (dicionario) map
+
+	Pointer
+		Variável que guarda o endereço de memória de outra variável
+
+	Conjunto
+		Set (util para operações SQL por conter operações semelhantes)
+
+
+*/
+
+public interface empilhavel {
+//C -> REATE (criar)
+//R -> EAD (ler) 
+//U -> PDATE (atualizar)
+//D -> ELETE (deletar/apagar)
+//S -> ORT (ordernar)
+//
+// Funções Básicas/Principais
+void empilhar(Object obj);  // push
+Object desempilhar();	    // pop	
+Object topo();		    // top
+void atualizar(Object obj); // update
+
+// Operações auxiliares
+Boolean estaCheia();        // isFull | overflow
+Boolean estaVazia();	    // isEmpty| underflow
+String imprimir();	    // print	
+
+
+}
