64821db43c6cbf80c3fe52f2f3e78275bfe058e2
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-03-17 23:23:38 -0300

    adição de placeholders para métodos de interface

--- primeiraUnidade/Aula2/PilhaDupla/PilhaDupla.java
+++ primeiraUnidade/Aula2/PilhaDupla/PilhaDupla.java
@@ -1,4 +1,41 @@
 package primeiraUnidade.Aula2.PilhaDupla;
 
-public class PilhaDupla {
+import primeiraUnidade.Aula1.Empilhavel;
+
+public class PilhaDupla implements Empilhavel {
+
+    @Override
+    public void empilhar(Object obj) {
+
+    }
+
+    @Override
+    public Object desempilhar() {
+        return null;
+    }
+
+    @Override
+    public Object topo() {
+        return null;
+    }
+
+    @Override
+    public void atualizar(Object obj) {
+
+    }
+
+    @Override
+    public Boolean estaCheia() {
+        return null;
+    }
+
+    @Override
+    public Boolean estaVazia() {
+        return null;
+    }
+
+    @Override
+    public String imprimir() {
+        return "";
+    }
 }
