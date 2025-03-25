951d5054546fcce4b0f4d8a7a8ae314371a901ad
kauê <atsimplantacaovdc@gmail.com>
2025-03-25 09:27:03 -0300

    Aula 03: definição de classe para Fila Estática Circular

---
+++ primeiraUnidade/Aula3/FilaEstaticaCircular/FilaEstaticaCircular.java
@@ -0,0 +1,112 @@
+package primeiraUnidade.Aula3.FilaEstaticaCircular;
+
+public class FilaEstaticaCircular implements Enfileiravel{
+    // variaveis de instancia
+    private Object[] dados;
+    private int ponteiroInicio; // head
+    private int ponteiroFim;    // tail
+    private int quantidade;
+
+    // construtores
+    public FilaEstaticaCircular() {
+        this(10);
+    }
+
+    public FilaEstaticaCircular(int tamanho) {
+        dados = new Object[tamanho];
+        ponteiroInicio = 0;
+        ponteiroFim = -1;
+        quantidade = 0;
+    }
+
+    // metodos principais
+    @Override
+    public void enfileirar(Object dado) {
+
+        if (!estaCheia()) {
+            ponteiroFim = (ponteiroFim+1)%dados.length;
+            dados[ponteiroFim] = dado;
+            quantidade++;
+        } else {
+            System.out.println("Fila está cheia");
+        }
+
+    };
+
+    @Override
+    public Object desenfileirar() {
+        Object frente = null;
+
+        if (!estaVazia()) {
+            frente = dados[ponteiroInicio];
+            ponteiroInicio = ((ponteiroInicio-1)+dados.length)%dados.length;
+
+            quantidade--;
+        } else {
+            System.out.println("Fila está vazia");
+        }
+
+        return frente;
+    }
+
+    @Override
+    public void atualizarInicio(Object dado) {
+        if (!estaVazia()) {
+            dados[ponteiroInicio] = dado;
+        } else {
+            System.err.println("Fila está vazia!");
+        }
+    }
+
+    @Override
+    public void atualizarFim(Object dado) {
+        if (!estaCheia()) {
+            dados[ponteiroFim] = dado;
+        } else {
+            System.err.println("Fila está cheia!");
+        }
+    }
+
+    public Object frente() {
+        Object frente = null;
+
+        if (!estaVazia()) {
+            frente = dados[ponteiroInicio];
+        } else {
+            System.out.println("Fila está vazia");
+        }
+
+        return frente;
+    }
+
+    // metodos auxiliares
+    @Override
+    public Boolean estaCheia() {
+        return (quantidade == dados.length);
+    }
+
+    @Override
+    public Boolean estaVazia() {
+        return (quantidade == 0);
+    }
+
+    @Override
+    public String imprimir() {
+        String retorno = "[";
+        for (int i = ponteiroInicio; i <= quantidade + ponteiroInicio; i++) {
+            /* funciona mas não é o mais adequado
+            if (i == dados.length) {
+                i = 0;
+            }
+            */
+
+            if (i == quantidade + ponteiroInicio - 1) {
+                retorno += dados[i%dados.length];
+            }
+            retorno += dados[i%dados.length] + ", ";
+        }
+        return retorno + "]";
+    }
+
+}
+
