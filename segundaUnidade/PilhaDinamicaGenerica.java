276685ecb2fd5e22e0ab49f6ddbf839e9dc8d0fc
Kauê Fernandes <90159661+fernandes-kaue@users.noreply.github.com>
2025-05-08 22:39:27 -0300

    metodos auxiliares

--- segundaUnidade/PilhaDinamicaGenerica.java
+++ segundaUnidade/PilhaDinamicaGenerica.java
@@ -30,6 +30,81 @@ public class PilhaDinamicaGenerica<T> implements Empilhavel<T> {
         this(10);
     }
 
-    
+    /**
+     * função que empilha um dado na pilha
+     * 
+     * @param dado a ser empilhado
+     * @throws NoSuchElementException se a piilha estiver cheia
+     */
+    @Override
+    public void empilhar(T dado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'empilhar'");
+    }
+
+    /**
+     * função que desempilha (remove) um dado da pilha
+     * 
+     * @return retorna o dado que foi desempilhado, podendo este ser reutilizado ou
+     *         descartado, à escolha do usuário
+     */
+    @Override
+    public T desempilhar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'desempilhar'");
+    }
+
+    /**
+     * função que retorna o dado do topo da pilha sem o remover da pilha
+     * 
+     * @return retorna o dado topo sem o remover da estrutura
+     */
+    @Override
+    public T espiar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'espiar'");
+    }
+
+    /**
+     * atualiza o topo da pilha
+     * 
+     * @param dado é o novo dado para o topo
+     */
+    @Override
+    public void atualizar(T dado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    /**
+     * função para saber se pilha está cheia
+     * 
+     * @return true se cheia, false se vazia
+     */
+    @Override
+    public boolean estaCheia() {
+        return (quantidade == tamanho);
+    }
+
+    /**
+     * função para saber se pilha está vazia
+     * 
+     * @return true se vazia, false se cheia
+     */
+    @Override
+    public boolean estaVazia() {
+        return (quantidade == 0);
+    }
+
+    /**
+     * função que retorna uma String com todos os dados da pilha
+     * 
+     * @return uma String com todos os dados da pilha, para posterior manipulação
+     */
+    @Override
+    public String imprimir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'imprimir'");
+    }
 
 }
