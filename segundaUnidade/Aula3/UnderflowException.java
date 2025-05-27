4a5379e318f67d21f20c29b3b8ac2f6887c2719b
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-05-26 22:48:11 -0300

    criação de exceções

---
+++ segundaUnidade/Aula3/UnderflowException.java
@@ -0,0 +1,30 @@
+package Aula3;
+
+/**
+ * exceção lançada quando operação tenta
+ * acessar/remover dados em uma estrutura vazia
+ * 
+ * @author Kauê Santos Fernandes
+ * @version 0.1
+ * @since 2025/05/26
+ */
+
+public class UnderflowException extends RuntimeException {
+
+    /**
+     * Construtor para criar uma exceção de subfluxo com uma mensagem específica.
+     *
+     * @param mensagem a mensagem de erro a ser exibida
+     */
+    public UnderflowException(String mensagem) {
+        super(mensagem);
+    }
+
+    /**
+     * Construtor padrão para criar uma exceção de subfluxo sem mensagem.
+     */
+    public UnderflowException() {
+        super("A lista está vazia ou a posição é inválida.");
+    }
+
+}
