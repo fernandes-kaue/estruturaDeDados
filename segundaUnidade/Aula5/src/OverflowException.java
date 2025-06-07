ac157754d2602b5965b3a0f683dfe75e11211afb
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-06-07 20:33:25 -0300

    heap: exceções

---
+++ segundaUnidade/Aula5/src/OverflowException.java
@@ -0,0 +1,30 @@
+package Aula3.src;
+
+/**
+ * exceção lançada quando operação tenta inserir dados em
+ * uma estrutura já cheia
+ * 
+ * @author Kauê Santos Fernandes
+ * @version 0.1
+ * @since 2025/06/07
+ */
+
+public class OverflowException extends RuntimeException {
+
+    /**
+     * Construtor para criar uma exceção de estouro com uma mensagem específica.
+     *
+     * @param mensagem a mensagem de erro a ser exibida
+     */
+    public OverflowException(String mensagem) {
+        super(mensagem);
+    }
+
+    /**
+     * Construtor padrão para criar uma exceção de estouro sem mensagem.
+     */
+    public OverflowException() {
+        super("A lista está cheia.");
+    }
+
+}
